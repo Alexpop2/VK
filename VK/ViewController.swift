b5c6ec15be69656472038263d476dfe2aef1c01c
alex7893 <alexqw1@ya.ru>
2019-01-26 18:27:39 +0300

    Initial Commit

---
+++ VK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Рабочий on 26/01/2019.
+//  Copyright © 2019 Рабочий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
